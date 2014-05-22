bd7d2cd62bd2106f27a9cedebf7e5042053dbdf8
jmmaxwell <johnymaxwell@gmail.com>
2014-05-22 12:27:25 -0700

    Draft 3

--- OriginDestinationPairsFunction.R
+++ OriginDestinationPairsFunction.R
@@ -5,8 +5,15 @@
 ## want to store resulting xlsx.
 
 
-origin.destination.pairs("FarmLocations.csv", "LewistonMcCoy.csv", 301, 350)
+origin.destination.pairs("FarmLocations.csv", "LewistonMcCoy.csv", 701, 900)
 
+origin.start = 301
+origin.stop = 350
+originCSV = "FarmLocations.csv"
+destinationCSV = "LewistonMcCoy.csv"
+c=1
+chunk.size = 25
+destination.size = 4
 
 origin.destination.pairs = function(originCSV, destinationCSV, origin.start, origin.stop){
   
@@ -55,7 +62,9 @@ origin.destination.pairs = function(originCSV, destinationCSV, origin.start, ori
     ### this block communicates with the API and get the request
     
     xml.url <- paste0('http://maps.googleapis.com/maps/api/distancematrix/xml?origins=',origin,'&destinations=',destination,'&mode=driving&sensor=false')
-    xmlfile <- xmlParse(getURL(xml.url))
+    xmlfile <- xmlTreeParse(xml.url)
+    xmltop = xmlRoot(xmlfile)
+    
     
     ### this block transforms the request into usable info
     
@@ -63,15 +72,22 @@ origin.destination.pairs = function(originCSV, destinationCSV, origin.start, ori
     dur.matrix = matrix(NA, nrow = chunk.size, ncol = destinations.size)
     final.dest.dist = c()
     final.dest.dur = c()
-    for (l in 1:chunk.size){
-      for (k in 1:destinations.size){
-        dist.matrix[l,k] <- as.numeric(xmlValue(xmlChildren(xpathApply(xmlfile,"//distance")[[(l-1)*destinations.size + k]])$value))
-        dur.matrix[l,k] <- as.numeric(xmlValue(xmlChildren(xpathApply(xmlfile,"//duration")[[(l-1)*destinations.size + k]])$value))
+    for (l in (2+destinations.size+chunk.size):(1+destinations.size+(chunk.size*2))){
+      if(xmlValue(xmlChildren(xpathApply(xmltop[l]$row,"//status")[[1]])$text)=="OK"){
+        for (k in 1:destinations.size){
+          dist.matrix[l-(1+destinations.size+chunk.size),k] = as.numeric(xmlValue(xmlChildren(xpathApply(xmltop[l]$row,"//distance")[[k]])$value))
+          #dist.matrix[l,k] <- as.numeric(xmlValue(xmlChildren(xpathApply(xmltop[l],"//distance")[[(l-1)*destinations.size + k]])$value))
+          dur.matrix[l-(1+destinations.size+chunk.size),k] = as.numeric(xmlValue(xmlChildren(xpathApply(xmltop[l]$row,"//duration")[[k]])$value))
+          #dur.matrix[l,k] <- as.numeric(xmlValue(xmlChildren(xpathApply(xmlfile,"//duration")[[(l-1)*destinations.size + k]])$value))
+        }
+      }else{
+        dist.matrix[l-(1+destinations.size+chunk.size),] = NA
+        dur.matrix[l-(1+destinations.size+chunk.size),] = NA
       }
-      final.dest.dist[l] = which(dist.matrix[l,]==min(dist.matrix[l,]))
-      final.dest.dur[l] = which(dur.matrix[l,]==min(dur.matrix[l,]))
+      #final.dest.dist[l-(1+destinations.size+chunk.size)] = which(dist.matrix[l-(1+destinations.size+chunk.size),]==min(dist.matrix[l-(1+destinations.size+chunk.size),]))
+      #final.dest.dur[l-(1+destinations.size+chunk.size)] = which(dur.matrix[l-(1+destinations.size+chunk.size),]==min(dur.matrix[l-(1+destinations.size+chunk.size),]))
     }
-    m.list[[c]] = data.table(dist.matrix, final.dest.dist, dur.matrix, final.dest.dur)
+    m.list[[c]] = data.table(dist.matrix, dur.matrix)
     
     Sys.sleep(10.1)
     
@@ -80,6 +96,32 @@ origin.destination.pairs = function(originCSV, destinationCSV, origin.start, ori
   ### this block returns info as a data.table
   
   final.data = rbindlist(m.list)
+  for (i in 1:destinations.size){
+    setnames(final.data, i, paste0("dist", i))
+    setnames(final.data, i+destinations.size, paste0("dur", i))
+  }
+  
+  min.dist = c()
+  for (i in 1:sample.size){
+    if (!is.na(final.data[,1,with=F][i])){
+      min.dist[i] = which(final.data[,1:destinations.size,with=F][i]==min(final.data[,1:destinations.size,with=F][i]))
+    }else{
+      min.dist[i]=NA
+    }
+  }
+  
+  min.dur = c()
+  for (i in 1:sample.size){
+    if (!is.na(final.data[,1,with=F][i])){
+      min.dur[i] = which(final.data[,destinations.size:destinations.size*2,with=F][i]==min(final.data[,destinations.size:destinations.size*2,with=F][i]))
+    }else{
+      min.dur[i]=NA
+    }
+  }
+  
+  final.data$min.dist = min.dist
+  final.data$min.dur = min.dur
+  
   write.xlsx(final.data, file = paste("Origins",origin.start, "to", origin.stop,".xlsx", sep = ''))
   return(final.data)
 }
